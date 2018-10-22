415c86e2dd1eaab8af9b8edebce65ec20eb1daf3
timkaiser <Tim.Kaiser@posteo.de>
2018-10-22 17:48:52 +0200

    Each objets uv coordinate are now rendered to their own rendertexture

---
+++ Assets/Scripts/UVCameraScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//this script makes sure that only one object gets rendered to the render target
+public class UVCameraScript : MonoBehaviour {
+
+
+    //Object that has to be rendered
+    [SerializeField]
+    private GameObject targetObject;
+
+    void Start() {
+        //Set camera layer to render layer
+        Camera camera = gameObject.GetComponent<Camera>();
+        camera.cullingMask = (1 << LayerMask.NameToLayer("UVRenderLayer"));
+        //set camera background
+        camera.clearFlags = CameraClearFlags.SolidColor;
+        camera.backgroundColor = Color.white;
+        gameObject.tag = "UVCamera";
+    }
+
+    //camera layer the object is currently in 
+    private int objectLayer = 0;
+
+    //Set the object that has to be rendered
+    public void SetTargetObject(GameObject o) {
+        targetObject = o;
+        objectLayer = o.layer;
+    }
+
+    //move object to render layer
+    //code source: https://answers.unity.com/questions/781931/render-one-object-to-each-camera.html [22.10.2018 - 16:40]
+    void OnPreCull() {
+        targetObject.layer = LayerMask.NameToLayer("UVRenderLayer");
+        
+    }
+
+    //move object back to original layer
+    void OnPostRender() {
+        targetObject.layer = objectLayer;
+    }
+}
---
+++ Assets/Scripts/UVCoordinateRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script renders the objects uv coordinates to  a render texture
+public class UVCoordinateRenderer : MonoBehaviour {
+    //Camera
+    [SerializeField]
+    private Camera camera;
+
+    //Render target for this camera
+    [SerializeField]
+    private RenderTexture renderTarget;
+
+    // Use this for initialization
+    void Start () {
+        //Find the main camera
+        GameObject mainCamera = GameObject.FindWithTag("MainCamera");
+
+        //clone the main camera
+        GameObject renderCamera = Instantiate((GameObject)Resources.Load("Prefabs/UVCamera"));
+        renderCamera.GetComponent<UVCameraScript>().SetTargetObject(gameObject);
+        renderCamera.transform.SetParent(mainCamera.transform);
+        camera = renderCamera.GetComponent<Camera>();
+
+        //set new render target to render texture
+        renderTarget = new RenderTexture(800,600,24);
+        camera.targetTexture = renderTarget;
+        
+    }
+}
